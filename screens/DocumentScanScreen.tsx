cb54611ef4a6ba386bdaa1e3f24893a74972f7a4
simonpojok <simonpojok@gmail.com>
2022-10-28 19:49:10 +0300

    install and configured react navigations

---
+++ screens/DocumentScanScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+export default function DocumentScanScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
