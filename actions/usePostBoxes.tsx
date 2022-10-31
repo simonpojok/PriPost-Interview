20e39043ef4a46c028a57b71c448021bc6f2f83e
simonpojok <simonpojok@gmail.com>
2022-11-01 00:21:39 +0300

    calling the poiny

--- actions/usePostBoxes.tsx
+++ actions/usePostBoxes.tsx
@@ -9,24 +9,30 @@ export default function usePostBoxes() {
   const [postBoxes, setPostBoxes] = useState<Array<PostBox>>([]);
   const [isPostBoxLoading, setIsPostBoxLoading] = useState(false);
   const [postBoxError, setPostBoxError] = useState({});
+  console.log('Prepared to call');
 
   useEffect(() => {
+    console.log('Prepared to calling');
     setIsPostBoxLoading(true);
-    axios
-      .get(POST_BOXES_URL, {
-        headers: {
-          Authorization: Config.AUTHORIZATION_BEARER_TOKEN,
-          Accept: 'application/json',
-        },
-      })
+    axios({
+      method: 'GET',
+      url: POST_BOXES_URL,
+      timeout: 10000,
+      headers: {
+        Authorization: Config.AUTHORIZATION_BEARER_TOKEN,
+        Accept: 'application/json',
+      },
+    })
       .then(response => {
+        console.log(response, 'RESPONSE');
         setPostBoxes(response.data);
       })
       .catch(error => {
-        console.log(error);
+        console.log('Error', error);
         setPostBoxError(error.message);
       })
       .finally(() => {
+        console.log('Prepared to called');
         setIsPostBoxLoading(false);
       });
   }, []);
